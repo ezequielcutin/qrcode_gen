8da41d987fae29cbdab3ecf5dbc0826f4c540389
ezequielcutin <93277070+ezequielcutin@users.noreply.github.com>
2022-05-11 11:38:36 -0400

    Python code

---
+++ main.py
@@ -0,0 +1,12 @@
+import qrcode
+
+details = qrcode.QRCode(version=1,box_size=30,border=4)
+website = input("enter website link here: ")
+print(website)
+details.add_data(website)
+details.make(fit=True)
+generateQR = details.make_image(fill_color="blue",back_color="yellow")
+generateQR.save(website + ".png")
+
+
+
